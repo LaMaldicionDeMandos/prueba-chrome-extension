8acc834e365db5450e744fe9e2a57cc337a9a09e
Marcelo Pasut <pasutmarcelo@gmail.com>
2014-11-17 16:27:16 -0300

    cambio alguna cosas para ande, y agrego listener para update de tabs

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
 chrome.tabs.onCreated.addListener(function(tab) {
-  appendToLog(
-      'tabs.onCreated -- window: ' + tab.windowId + ' tab: ' + tab.id +
+  console.log('tabs.onCreated -- window: ' + tab.windowId + ' tab: ' + tab.id +
       ' title: ' + tab.title + ' index ' + tab.index + ' url ' + tab.url);
-  loadWindowList();
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status == 'loading') {
+  	console.log('tabs.onUpdated -- window: ' + tab.windowId + ' tab: ' + tab.id +
+      ' title: ' + tab.title + ' index ' + tab.index + ' url ' + tab.url);  	
+  }
+
 });
